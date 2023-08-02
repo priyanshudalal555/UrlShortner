19d8812da1c46d08c014648ffdf7dd513d809725
Priyanshu Dalal <62369741+priyanshudalal555@users.noreply.github.com>
2023-08-02 16:08:54 +0530

    Add files via upload

---
+++ copy.js
@@ -0,0 +1,7 @@
+let newURL = document.getElementById("shorturl");
+let coptButton = document.getElementById("copy");
+
+coptButton.onclick = ()=>{
+    newURL.select();
+    window.navigator.clipboard.writeText(newURL.value);
+}
